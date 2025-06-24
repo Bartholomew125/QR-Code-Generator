250f7ea75d45b81c57832f348b7258f74dca0ad6
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-06-24 13:57:38 +0200

    Created InvalidVersionForAlignmentPatternException
    This exception is used if alignment patterns are trying to be found for
    QR code version 2.

---
+++ exceptions/InvalidVersionForAlignmentPatternException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+/**
+ * This exception is used whenever the QR code version is 2, but and alignment
+ * pattern is trying to be found for it. This is because the only QR code
+ * version that doesn't have alignment patterns is version 2.
+ */
+public class InvalidVersionForAlignmentPatternException extends Exception {
+    
+    public InvalidVersionForAlignmentPatternException(int version) {
+        super("Invalid version "+version+" for alignment pattern.");
+    }
+}
