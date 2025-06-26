30b353969d53baf85866747c5c1a43b788f7fd8f
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-06-26 19:03:57 +0200

    Created InvalidVersionException
    This is an exception that is used whenever an invalid QR code version is
    passed.

---
+++ exceptions/InvalidVersionException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+/**
+ * InvalidVersionException is a class that extends Exception. This exception is
+ * to be used whenever an invalid QR code version is used. The only valid QR
+ * code version are 1 through 40.
+ */
+public class InvalidVersionException extends Exception {
+
+    public InvalidVersionException() {
+        super("Invalid QR code version. Must be between 1 and 40 inclusive.");
+    }
+}
