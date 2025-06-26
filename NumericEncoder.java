dc0bd1282dd20f8d818d4e1e390f648c3b1d6486
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-06-26 22:03:21 +0200

    Created NumericEncoder
    This class is used for encoding data that is of the numeric encoding
    mode.

---
+++ NumericEncoder.java
@@ -0,0 +1,31 @@
+/**
+ * NumericEncoder is a class that is used to encode numeric data.
+ */
+public class NumericEncoder {
+    
+    /**
+     * Returns the encoded data.
+     */
+    public static String encode(String data) {
+        // Put data into groups
+        String[] groups = new String[(data.length()+2)/3];
+        int groupIndex = 0;
+        for (int i = 0; i < data.length(); i++) {
+            groups[groupIndex] += data.substring(i, i+1);
+            if (i%3 == 2) {
+                groupIndex++;
+            }
+        }
+        // Remove leading zeros in groups
+        int[] finalGroups = new int[groups.length];
+        for (int i = 0; i < finalGroups.length; i++) {
+            finalGroups[i] = Integer.valueOf(groups[i]);
+        }
+        // Convert to binary
+        String result = "";
+        for (int group : finalGroups) {
+            result = result + Converter.decimalToBinary(group);
+        }
+        return result;
+    }
+}
