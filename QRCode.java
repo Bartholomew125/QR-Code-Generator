40cdc67830776196746688ad9b54c20a37b1f7dd
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-06-26 22:10:34 +0200

    Created QRCode
    This class simply represents the QR code and all its data.

---
+++ QRCode.java
@@ -0,0 +1,19 @@
+/**
+ * QRCode is a class that represents a QRCode.
+ */
+public class QRCode {
+
+    private String data;
+    private String bits;
+    private EncodingMode encodingMode;
+    private ErrorCorrectionLevel errorCorrectionLevel;
+    private int version;
+
+    public QRCode(String data, String bits, EncodingMode encodingMode, ErrorCorrectionLevel errorCorrectionLevel, int version) {
+        this.data = data;
+        this.bits = bits;
+        this.encodingMode = encodingMode;
+        this.errorCorrectionLevel = errorCorrectionLevel;
+        this.version = version;
+    }
+}
