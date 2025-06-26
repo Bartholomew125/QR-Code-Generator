1242b2f7e611cf543d6d6f7998ab7a104e978a50
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-06-26 19:18:42 +0200

    Created EncodingMode
    This is a enum that holds the different kinds of encoding modes for the
    QR code.

---
+++ EncodingMode.java
@@ -0,0 +1,10 @@
+/**
+ * EncodingMode is a enum that holds the different kinds of encoding modes for
+ * the QR code.
+ */
+public enum EncodingMode {
+    Numeric,
+    Alphanumeric,
+    Byte,
+    Kanji
+}
