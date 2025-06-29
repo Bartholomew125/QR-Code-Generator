d33e23841c4a68980b5bbe83a13d7fc704f885ae
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-06-29 12:14:13 +0200

    Fixed wrong range in valid mask patterns

--- Exceptions/InvalidMaskPatternException.java
+++ Exceptions/InvalidMaskPatternException.java
@@ -3,12 +3,12 @@ package Exceptions;
 /**
  * InvalidMaskPatternException is a class that extends Exception. This exception
  * is to be used whenever an attempt is made to use a mask pattern outside the
- * predefined range.
+ * predefined range of 0 through 7.
  */
 public class InvalidMaskPatternException extends Exception {
 
     public InvalidMaskPatternException() {
-        super("Invalid mask pattern. Must be between 1 and 7 inclusive.");
+        super("Invalid mask pattern. Must be between 0 and 7 inclusive.");
     }
     
 }
