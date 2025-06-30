aadef9e9f57a266ef329be8cdb2b0e471e298ac9
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-06-30 10:31:47 +0200

    Changed check method
    Created custom method for checking if all characters in string are in
    another string, which simplifies the logic.

--- Encoding/EncodingModeAnalyser.java
+++ Encoding/EncodingModeAnalyser.java
@@ -1,9 +1,4 @@
-import java.util.List;
-
 import Types.EncodingMode;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 
 /**
  * EncodingModeAnalyser is a class that is used for analysing what type of
@@ -11,32 +6,43 @@ import java.util.Arrays;
  */
 public class EncodingModeAnalyser {
 
-    private static final List<String> NUMERIC = Arrays.asList("0123456789".split(""));
-    private static final List<String> ALPHANUMERIC = Arrays.asList("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:".split(""));
-    private static final List<String> BYTE = EncodingModeAnalyser.getByteCharacters();
+    private static final String NUMERIC = "0123456789";
+    private static final String ALPHANUMERIC = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:";
+    private static final String BYTE = EncodingModeAnalyser.getByteCharacters();
 
     /**
      * Returns all the byte characters.
      */
-    private static List<String> getByteCharacters() {
-        List<String> characters = new ArrayList<>();
+    private static String getByteCharacters() {
+        String characters = "";
         for (int i = 0; i < 256; i++) {
-            characters.add(String.valueOf((char) i));
+            characters += String.valueOf((char) i);
         }
         return characters;
+    }
+
+    /**
+     * Checks whether every character in the data string is contained in the
+     * checkString.
+     */
+    private static boolean allIn(String data, String checkString) {
+        for (String c : data.split("")) {
+            if (!checkString.contains(c)) { return false; }
+        }
+        return true;
     }
 
     /**
      * Returns the encoding mode that should be used for the given data.
      */
     public static EncodingMode analyse(String data) {
-        if (data.chars().allMatch(c -> NUMERIC.contains(c))) {
+        if (allIn(data, NUMERIC)) {
             return EncodingMode.Numeric;
         }
-        if (data.chars().allMatch(c -> ALPHANUMERIC.contains(c))) {
+        if (allIn(data, ALPHANUMERIC)) {
             return EncodingMode.Alphanumeric;
         }
-        if (data.chars().allMatch(c -> BYTE.contains(c))) {
+        if (allIn(data, BYTE)) {
             return EncodingMode.Byte;
         }
         return EncodingMode.Kanji;
