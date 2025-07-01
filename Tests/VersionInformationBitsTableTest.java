3f91983e36395419565d406265ab38e90b0c887a
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-07-01 13:36:52 +0200

    Added test for exception

--- Tests/VersionInformationBitsTableTest.java
+++ Tests/VersionInformationBitsTableTest.java
@@ -1,6 +1,7 @@
 package Tests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 import org.junit.Test;
 
@@ -16,5 +17,11 @@ public class VersionInformationBitsTableTest {
         assertEquals(vibt.get(20), "010100100110100110");
         assertEquals(vibt.get(30), "011110110101110101");
         assertEquals(vibt.get(40), "101000110001101001");
+    }
+
+    @Test public void testVersionInformationBitsTableException() {
+        VersionInformationBitsTable vibt = new VersionInformationBitsTable();
+        assertThrows(Exception.class, () -> vibt.get(6));
+        assertThrows(Exception.class, () -> vibt.get(41));
     }
 }
