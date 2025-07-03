908c020f5eeca8c120d802a345c884872ff1aabc
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-07-03 20:37:02 +0200

    Created test for Converter

---
+++ Tests/ConverterTest.java
@@ -0,0 +1,19 @@
+package Tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import Util.Converter;
+
+public class ConverterTest {
+    
+    @Test
+    public void testConverterDecimalToBinary() {
+        assertEquals(Converter.decimalToBinary(2), "10");
+        assertEquals(Converter.decimalToBinary(16), "10000");
+        assertEquals(Converter.decimalToBinary(127), "1111111");
+        assertEquals(Converter.decimalToBinary(73829), "10010000001100101");
+        assertEquals(Converter.decimalToBinary(1), "1");
+    }
+}
